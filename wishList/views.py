d2a42e4bd06497ae3e6f2ff044216fa514abbf8f
Daniel Rangel <drangelfigueroa@csumb.edu>
2021-03-06 14:34:42 -0800

    Added sign up page

--- wishList/views.py
+++ wishList/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     context = {}
-    return render(request, 'home.html', context)
+    return render(request, '../templates/home.html', context)
+def createAcc(request):
+    context = {}
+    return render(request, '../templates/createAcc.html', context)
