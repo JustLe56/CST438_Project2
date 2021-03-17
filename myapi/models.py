76c02f2620f01f5bf2ec7763449e70f506817670
Christian Sumares <christiansumares@gmail.com>
2021-03-16 18:40:30 -0700

    Created database models  - Created Wishlist, Link, and WishlistItem models  - Made migrations for database  - Made tests for these models

--- myapi/models.py
+++ myapi/models.py
@@ -3,7 +3,27 @@ from django.db import models
 
 
 class WishListUser(AbstractUser):
-	pass
 
 	def __str__(self):
 		return self.username
+
+
+class Wishlist(models.Model):
+	wishlistUser = models.ForeignKey(WishListUser, on_delete=models.CASCADE)
+
+
+class Link(models.Model):
+	url = models.URLField(unique=True)
+
+
+class WishlistItem(models.Model):
+	wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE)
+	link = models.ForeignKey(Link, on_delete=models.CASCADE)
+	name = models.CharField(max_length=100)
+	description = models.CharField(blank=True, max_length=1000)
+	image_url = models.URLField(blank=True)
+	priority = models.PositiveSmallIntegerField()
+	index = models.PositiveSmallIntegerField()
+
+	class Meta:
+		unique_together = ['wishlist', 'index']
--- myapi/tests.py
+++ myapi/tests.py
@@ -1,8 +1,10 @@
 from rest_framework import status
 from rest_framework.test import APITestCase, APIRequestFactory
-from .models import WishListUser
+from .models import WishListUser, Wishlist, Link, WishlistItem
 from .serializers import WishListUserSerializer
 from json import loads as json_loads
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 
 
 class WishListUserTests(APITestCase):
@@ -40,3 +42,72 @@ class WishListUserTests(APITestCase):
         response = self.client.post('/api/login/', test_incorrect_credentials, format='json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(json_loads(response.content), {'detail': 'Invalid credentials'})
+
+
+class WishlistTests(APITestCase):
+
+    def setUp(self):
+        self.user = WishListUserSerializer().create({'username': 'Lars', 'password': 'n4p573r5uk5'})
+        self.wishlist_properties = {'wishlistUser': self.user}
+
+    def test_create_wishlist_instance(self):
+        wishlist = Wishlist(**self.wishlist_properties)
+        try:
+            wishlist.save()
+        except ValidationError as e:
+            self.fail(e.message)
+
+
+class LinkTests(APITestCase):
+
+    def setUp(self):
+        self.link_properties = {
+            'url': 'https://www.bkstr.com/csumontereybaystore/product/clothing-accessories/men/t-shirts-tanks-men/calif'
+                   'ornia-state-university-monterey-bay-short-sleeve-t-shirt-213389-1'
+        }
+
+    def test_create_link_instance(self):
+        link = Link(**self.link_properties)
+        try:
+            link.save()
+        except ValidationError as e:
+            self.fail(e.message)
+
+    def test_link_url_unique(self):
+        link = Link(**self.link_properties)
+        link.save()
+        duplicate = Link(**self.link_properties)
+        self.assertRaises(IntegrityError, duplicate.save)
+
+
+class WishlistItemTests(APITestCase):
+
+    def setUp(self):
+        self.user = WishListUserSerializer().create({'username': 'Kirk', 'password': 'w4hw4h'})
+        self.wishlist = Wishlist(wishlistUser=self.user)
+        self.wishlist.save()
+        self.link = Link(url='https://lazerhawk.bandcamp.com/merch/2x-lazerhawk-logo-t-shirt-sale')
+        self.link.save()
+        self.item_properties = {
+            'wishlist': self.wishlist,
+            'link': self.link,
+            'name': 'Lazerhawk T-Shirt',
+            'description': 'An awesome T-Shirt.',
+            'image_url': 'https://f4.bcbits.com/img/0003534389_10.jpg',
+            'priority': 10,
+            'index': 0
+        }
+
+    def test_create_wishlist_item_instance(self):
+        item = WishlistItem(**self.item_properties)
+        try:
+            item.save()
+        except ValidationError as e:
+            self.fail(e.message)
+
+    def test_wishlist_and_index_unique(self):
+        item = WishlistItem(**self.item_properties)
+        item.save()
+        item_duplicate_index = WishlistItem(**self.item_properties)
+        item_duplicate_index.name = 'Lazerhawk T-Shirt 2'
+        self.assertRaises(IntegrityError, item_duplicate_index.save)
