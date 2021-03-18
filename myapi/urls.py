46ba1e0208b0a3c3ab08e523e2344d312a5ab689
Nayan <ngupta@csumb.edu>
2021-03-18 13:06:13 -0700

    Tests and bugfixes

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CreateWishListUser, api_login_view
+from .views import CreateWishListUser, api_login_view, api_logout_view
 
 urlpatterns = [
     path('createAccount/', CreateWishListUser.as_view()),
     path('login/', api_login_view),
-    path('logout/' api_logout_view)
+    path('logout/', api_logout_view)
 ]
