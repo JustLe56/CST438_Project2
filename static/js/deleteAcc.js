2aaec992f56a87df8780782162672cc3fa60f45a
Justin Le <jule@csumb.edu>
2021-03-25 18:39:49 -0700

    added redirection to create account page

--- static/js/deleteAcc.js
+++ static/js/deleteAcc.js
@@ -1,5 +1,21 @@
 function deleteAccount(){
     $("#delete").submit(async function (event) {
+        event.preventDefault();
+        let $form = $(this),
+            url = $form.attr("action"),
+            csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
+
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify()
+        };
+
+        let response = await fetch(url, options);
+        const json = await response.json()
         $("#success").submit();
-    }
+    });
 }
